f34ca8e84f3ce3db7c2e77e6973a48127d842ca5
Siddharth Surapaneni <siddharthsurapaneni@Siddharths-MBP.home>
2020-07-31 08:51:13 -0400

    tried plotly

--- Visualization.py
+++ Visualization.py
@@ -39,42 +39,42 @@ data=data.sort_values('time').round(2)
 
 
 # And I need to transform my categorical column (continent) in a numerical value group1->1, group2->2...
-data['Region']=pd.Categorical(data['Region'])
+#data['Region']=pd.Categorical(data['Region'])
 
 #Needs fitting with datetime
 # For each year:
 for i in sorted(data["time"].unique()):
 
 # initialize a figure
-    fig = plt.figure(figsize=(680/my_dpi, 480/my_dpi), dpi=my_dpi)
+    #fig = plt.figure(figsize=(680/my_dpi, 480/my_dpi), dpi=my_dpi)
     
 # Change color with c and alpha. I map the color to the X axis value.
     tmp=data[ data["time"] == i ]
-    
+    tmp=tmp.sort_values("Region")
     #sns.relplot(x='Percent Population on Welfare',y='Adjusted Benefits Per Beneficiary',hue='Region',size=tmp["Population"],alpha=0.6,data=tmp,palette='Set2',sizes=(200,2000),height=7)
-    plt.scatter(tmp['Percent Population on Welfare'], tmp['Adjusted Benefits Per Beneficiary'] , s=tmp['Population']/1000 , c=tmp['Region'].cat.codes, cmap="Accent", alpha=0.6, edgecolors="white", linewidth=2)
+    #plt.scatter(tmp['Percent Population on Welfare'], tmp['Adjusted Benefits Per Beneficiary'] , s=tmp['Population']/1000 , c=tmp['Region'].cat.codes, cmap="Accent", alpha=0.6, edgecolors="white", linewidth=2)
     #plt.annotate(tmp[tmp["Region"]=="Capital"]["Region"][0],(tmp[tmp["Region"]=="Capital"]['Percent Population on Welfare'][0],tmp[tmp["Region"]=="Capital"]['Adjusted Benefits Per Beneficiary'][0]))
 # Add titles (main and on axis)
 #plt.yscale('log')
     the_title=str(tmp["Month"].unique()[0])
-    plt.xlabel("Percent Population on Welfare")
-    plt.ylabel("Adjusted Benefits Per Beneficiary")
-    plt.title(the_title)
-    plt.ylim(200,2000)
-    plt.xlim(0, 12)
+    #plt.xlabel("Percent Population on Welfare")
+    # plt.ylabel("Adjusted Benefits Per Beneficiary")
+    # plt.title(the_title)
+    # plt.ylim(200,2000)
+    # plt.xlim(0, 12)
     
     
-    #fig= px.scatter(tmp, x="Percent Population on Welfare", y="Adjusted Benefits Per Beneficiary", color='Region',size='Population',size_max=75,title=the_title)
-    #fig.update_yaxes(range=[200,2000])
-    #fig.update_xaxes(range=[0, 12])
-    #fig.update_traces(marker=dict(opacity=0.6))
+    fig= px.scatter(tmp, x="Percent Population on Welfare", y="Adjusted Benefits Per Beneficiary", color='Region',size='Population',size_max=50,title=the_title,color_discrete_sequence=px.colors.qualitative.Dark24)
+    fig.update_yaxes(range=[200,2000])
+    fig.update_xaxes(range=[0, 12])
+    fig.update_traces(marker=dict(opacity=0.5))
     #plt.legend(tmp['Region'].cat.codes,tmp['Region'])
 
 # Save it
     filename='Unemployment attempt'+str(i)+'.png'
-    #fig.write_image(filename)
-    plt.savefig(filename, dpi=96)
-    plt.gca()
+    fig.write_image(filename)
+    # plt.savefig(filename, dpi=96)
+    # plt.gca()
 
 
 #Then use image magick (this is bash, not python)
