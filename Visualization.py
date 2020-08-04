f598a9741e1b02b7ba10137c49318419e0c0862a
Siddharth Surapaneni <60629151+ssiddharth2@users.noreply.github.com>
2020-08-03 20:36:16 -0400

    update

--- Visualization.py
+++ Visualization.py
@@ -43,20 +43,22 @@ data=data.sort_values('time').round(2)
 
 #Needs fitting with datetime
 # For each year:
+data=data[data["time"]<=dt.datetime(2020,2,1)]
 for i in sorted(data["time"].unique()):
 
 # initialize a figure
     #fig = plt.figure(figsize=(680/my_dpi, 480/my_dpi), dpi=my_dpi)
-    
 # Change color with c and alpha. I map the color to the X axis value.
     tmp=data[ data["time"] == i ]
     tmp=tmp.sort_values("Region")
+    #print(type(i))
     #sns.relplot(x='Percent Population on Welfare',y='Adjusted Benefits Per Beneficiary',hue='Region',size=tmp["Population"],alpha=0.6,data=tmp,palette='Set2',sizes=(200,2000),height=7)
     #plt.scatter(tmp['Percent Population on Welfare'], tmp['Adjusted Benefits Per Beneficiary'] , s=tmp['Population']/1000 , c=tmp['Region'].cat.codes, cmap="Accent", alpha=0.6, edgecolors="white", linewidth=2)
     #plt.annotate(tmp[tmp["Region"]=="Capital"]["Region"][0],(tmp[tmp["Region"]=="Capital"]['Percent Population on Welfare'][0],tmp[tmp["Region"]=="Capital"]['Adjusted Benefits Per Beneficiary'][0]))
 # Add titles (main and on axis)
 #plt.yscale('log')
-    the_title=str(tmp["Month"].unique()[0])
+  
+    the_title= str(tmp["Month"].unique()[0])
     #plt.xlabel("Percent Population on Welfare")
     # plt.ylabel("Adjusted Benefits Per Beneficiary")
     # plt.title(the_title)
@@ -66,7 +68,7 @@ for i in sorted(data["time"].unique()):
     
     fig= px.scatter(tmp, x="Percent Population on Welfare", y="Adjusted Benefits Per Beneficiary", color='Region',size='Population',size_max=50,title=the_title,color_discrete_sequence=px.colors.qualitative.Dark24)
     fig.update_yaxes(range=[200,2000])
-    fig.update_xaxes(range=[0, 12])
+    fig.update_xaxes(range=[0, 4])
     fig.update_traces(marker=dict(opacity=0.5))
     #plt.legend(tmp['Region'].cat.codes,tmp['Region'])
 
@@ -78,6 +80,6 @@ for i in sorted(data["time"].unique()):
 
 
 #Then use image magick (this is bash, not python)
-#convert -delay 80 Unemployment*.png animated_gapminder.gif
+#convert -delay 20 Unemployment*.png animated_gapminder.gif
 
 
